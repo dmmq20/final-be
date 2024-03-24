b92fdb592793b9d64fe7d805d1585c1b494c5a3a
HarryHadcroft <harry.hadcroft@delmdigital.co.uk>
2024-03-24 17:58:38 +0000

    Added test for get_users function

---
+++ tests/test_main.py
@@ -0,0 +1,18 @@
+import pytest
+from app.main import app
+from fastapi.testclient import TestClient
+
+client = TestClient(app)
+
+
+def test_get_users():
+    response = client.get("/users")
+    assert response.status_code == 200
+    users_data = response.json()
+    assert len(users_data) == 2
+    expected_keys = ["id", "created_at", "username"]
+    for user in users_data:
+        assert all(key in user for key in expected_keys)
+
+
+
