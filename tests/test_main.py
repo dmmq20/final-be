c3b43e7170ebb1339f61d8bcb012c15f7d7c7ef3
HarryHadcroft <harry.hadcroft@delmdigital.co.uk>
2024-03-24 18:15:37 +0000

    added testing for get_user endpoint

--- tests/test_main.py
+++ tests/test_main.py
@@ -10,9 +10,17 @@ def test_get_users():
     assert response.status_code == 200
     users_data = response.json()
     assert len(users_data) == 2
-    expected_keys = ["id", "created_at", "username"]
+    expected_keys = ["id", "created_at", "username", "first_name"] # not currently testing for instances where the first_name is not present
     for user in users_data:
         assert all(key in user for key in expected_keys)
+
+
+def test_get_user():
+    response = client.get("/users/1")
+    user_data = response.json()
+    assert response.status_code == 200
+    expected_keys = ["id", "created_at", "username", "first_name"] # not currently testing for instances where the first_name is not present
+    assert all(key in user_data for key in expected_keys)
 
 
 
