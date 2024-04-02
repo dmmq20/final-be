b0559ff3d5ee2db7cc01db19bfee0a44e5d388ae
HarryHadcroft <harry.hadcroft@delmdigital.co.uk>
2024-04-02 15:54:03 +0100

    added functionality to see a documents a user is collaborating on

--- app/crud/collaborations.py
+++ app/crud/collaborations.py
@@ -1,19 +1,25 @@
 from fastapi import HTTPException, status
 from app.db import init_db
 
-from app.models import Collaboration
+from app.models import DocumentCollaboration, UserCollaboration
 
 
-def row_to_model(row) -> Collaboration:
+def document_row_to_model(row) -> DocumentCollaboration:
     id, document_id, user_id, username = row
-    return Collaboration(id=id, document_id=document_id, user_id=user_id, username=username)
+    return DocumentCollaboration(id=id, document_id=document_id, user_id=user_id, username=username)
+
+def user_row_to_model(row) -> UserCollaboration:
+    id, document_id, user_id, title = row
+    return UserCollaboration(id=id, document_id=document_id, user_id=user_id, title=title)
+
 
 def get_collaborations_by_id(id):
     with init_db as db:
-        db.cursor.execute("SELECT collaborations.*, users.username FROM collaborations JOIN users ON collaborations.user_id = users.id WHERE collaborations.id = %s;", (id,))
+        db.cursor.execute(
+            "SELECT collaborations.*, users.username FROM collaborations JOIN users ON collaborations.user_id = users.id WHERE collaborations.id = %s;", (id,))
         collaborations_data = db.cursor.fetchone()
         db.connection.commit()
-    return row_to_model(collaborations_data)
+    return document_row_to_model(collaborations_data)
 
 
 def get_collaborations_by_document_id(document_id):
@@ -22,9 +28,10 @@ def get_collaborations_by_document_id(document_id):
             "SELECT collaborations.*, users.username FROM collaborations JOIN users ON collaborations.user_id = users.id WHERE collaborations.document_id = %s;", (document_id,))
         collaborations_data = db.cursor.fetchall()
         db.connection.commit()
-    return [row_to_model(collaboration) for collaboration in collaborations_data]
+    return [document_row_to_model(collaboration) for collaboration in collaborations_data]
 
-def post_collaborations(document_id, collaboration: Collaboration) -> Collaboration:
+
+def post_collaborations(document_id, collaboration: DocumentCollaboration) -> DocumentCollaboration:
     data = collaboration.model_dump()
     with init_db as db:
         query = "INSERT INTO collaborations (document_id, user_id) VALUES (%s, %s) RETURNING *;"
@@ -35,3 +42,13 @@ def post_collaborations(document_id, collaboration: Collaboration) -> Collaborat
         new_collaboration = get_collaborations_by_id(inserted_id)
         return new_collaboration
 
+
+def get_collaborations_by_user_id(user_id):
+    with init_db as db:
+        db.cursor.execute(
+            "SELECT collaborations.*, documents.title FROM collaborations JOIN documents ON collaborations.document_id = documents.id WHERE collaborations.user_id = %s;", (
+                user_id,))
+        collaborations_data = db.cursor.fetchall()
+        print(collaborations_data)
+        db.connection.commit()
+    return [user_row_to_model(collaboration) for collaboration in collaborations_data]
--- app/web/collaborations.py
+++ app/web/collaborations.py
@@ -2,14 +2,19 @@ from fastapi import APIRouter, status
 from fastapi.exceptions import HTTPException
 
 from app.services import collaborations as service
-from app.models import Collaboration
+from app.models import DocumentCollaboration, UserCollaboration
 
-router = APIRouter(prefix='/documents')
+router = APIRouter(prefix='')
 
-@router.get('/{document_id}/collaborations')
-async def get_collaborations_by_document(document_id: int) -> list[Collaboration]:
-    return service.get_some(document_id)
+@router.get('/documents/{document_id}/collaborations')
+async def get_collaborations_by_document(document_id: int) -> list[DocumentCollaboration]:
+    return service.document_get_some(document_id)
 
-@router.post('/{document_id}/collaborations')
-async def add_collaboration(document_id, collaboration: Collaboration) -> Collaboration:
+@router.get('/users/{user_id}/collaborations')
+async def get_collaborations_by_users(user_id: int) -> list[UserCollaboration]:
+    return service.user_get_some(user_id)
+
+
+@router.post('/documents/{document_id}/collaborations')
+async def add_collaboration(document_id, collaboration: DocumentCollaboration) -> DocumentCollaboration:
     return service.create(document_id, collaboration)
