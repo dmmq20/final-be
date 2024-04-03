d2b47ae38e82c7b7674e02aa246c16549e1f2adb
HarryHadcroft <harry.hadcroft@delmdigital.co.uk>
2024-04-03 16:58:12 +0100

    refactored comments to show users avatar

--- app/crud/comments.py
+++ app/crud/comments.py
@@ -3,10 +3,12 @@ from app.db import init_db
 
 from app.models import Comment
 
+
 def row_to_model(row) -> Comment:
-    id, author, document_id, content, created_at = row
+    id, author, document_id, content, created_at, avatar_url = row
     return Comment(id=id, author=author, document_id=document_id, content=content,
-                    created_at=created_at)
+                   created_at=created_at, avatar_url=avatar_url)
+
 
 def get_comment_by_id(id):
     with init_db as db:
@@ -15,12 +17,17 @@ def get_comment_by_id(id):
         db.connection.commit()
     return row_to_model(comment_data)
 
+
 def get_comments_by_document_id(document_id):
     with init_db as db:
-        db.cursor.execute("SELECT * FROM comments WHERE document_id = %s", (document_id,))
+        db.cursor.execute("""SELECT comments.*, users.avatar_url
+        FROM comments
+        JOIN users ON comments.author = users.username
+        WHERE comments.document_id = %s;""", (document_id,))
         comment_data = db.cursor.fetchall()
         db.connection.commit()
     return [row_to_model(comment) for comment in comment_data]
+
 
 def post_comment_by_document_id(document_id, comment: Comment) -> Comment:
     data = comment.model_dump()
@@ -32,9 +39,11 @@ def post_comment_by_document_id(document_id, comment: Comment) -> Comment:
         db.connection.commit()
         new_comment = get_comment_by_id(inserted_id)
         return new_comment
-    
+
+
 def delete_comment_by_id(comment_id):
     with init_db as db:
-        db.cursor.execute("DELETE FROM comments WHERE id = %s RETURNING *;", (comment_id,))
+        db.cursor.execute(
+            "DELETE FROM comments WHERE id = %s RETURNING *;", (comment_id,))
         db.connection.commit()
     return {"message": "comment successfully deleted"}
