25e81bf2168bdbfda18d9c33115d3ef7ed174031
HarryHadcroft <harry.hadcroft@delmdigital.co.uk>
2024-04-04 09:37:30 +0100

    changed avatar for 1 user

--- app/db/data/users.py
+++ app/db/data/users.py
@@ -4,7 +4,7 @@ userData = [("Edgar Burke", hash_password("secret123"), "https://miro.medium.com
             ("Eloy Erikson", hash_password("password123"),
              "https://assets-global.website-files.com/636b968ac38dd1495ec4edcd/63ce5755b45e867c12c9b3cb_socials_profile.webp"),
             ("Claire Hayes", hash_password("mypassword"),
-             "https://assets-global.website-files.com/61dc0796f359b6eeecc06eab/65cb7547daa399524bc2979c_6399fa76f26ed4adf7595d62_paige_1.png"),
+             "https://fiverr-res.cloudinary.com/videos/t_main1,q_auto,f_auto/yincg3fgxrnere4tgf6d/design-a-bespoke-ai-avatar-to-enhance-your-brands-appeal.png"),
             ("Martha Robertson", hash_password("strongpassword"),
              "https://pfpmaker.com/images/ai/examples/first/results/result-pic-1.png"),
             ("Todd Hoover", hash_password("verysecure"), "https://preview.redd.it/transform-your-selfie-into-a-stunning-ai-avatar-with-stable-v0-2z411m9dob6a1.png?width=1024&format=png&auto=webp&s=8e97db53d8c34044ce841f0110cf996fbd28f006")]
